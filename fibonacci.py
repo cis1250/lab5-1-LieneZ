08f4172da11ffd4ac69d201fc096724887698840
Liene Zubans <lzubans@uoguelph.ca>
2025-10-21 10:49:47 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,32 +3,41 @@
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
 
-num = -1
-while num <= 0:
-    try:
-        num = int(input("How many Fibonacci terms do you want? "))
-        if (num <= 0):
+def positive():
+    num = -1
+    while num < 0:
+        try:
+            num = int(input("How many Fibonacci terms do you want? "))
+            if (num < 0):
+                print("Invalid input: please enter a positive integer.")
+            else:
+                continue
+        except ValueError:
             print("Invalid input: please enter a positive integer.")
+
+    return num
+
+def fibonacci_seq(number):
+    number = int(number)
+    term2 = 1
+    output = 1
+
+    for i in range(0, int(number)):
+        if i == 0:
+            output = 0
+        elif i == 1 or i == 2:
+            output = 1
         else:
-            continue
-    except ValueError:
-        print("Invalid input: please enter a positive integer.")
+            term1 = term2
+            term2 = output
+
+            output = term1 + term2
+        print_fib(output)
+
+def print_fib(out):
+    print(out, end=' ')
 
 
 
-num = int(num)
+fibonacci_seq(positive())
 
-term2 = 1
-output = 1
-
-for i in range(0, int(num)):
-    if i == 0:
-        output = 0
-    elif i == 1 or i == 2:
-        output = 1
-    else:
-        term1 = term2
-        term2 = output
-
-        output = term1 + term2
-    print(output, end=' ')
